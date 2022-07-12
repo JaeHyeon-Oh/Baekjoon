d1fe1d27b66a6397ba9f874e23c0a9affb1245b8
Jaehyeon Oh <dhwogus1212@naver.com>
2022-07-12 23:32:10 +0900

    Fourth DP Problem

---
+++ DP/전깃줄.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin=open('D:/Baekjoon/input.txt')
+n=int(sys.stdin.readline())
+a=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+a.sort()
+d=[1]*101
+for i in range(n):
+    for j in range(i):
+        if a[i][1]>a[j][1]:
+            d[i]=max(d[i],d[j]+1)
+print(n-max(d))
