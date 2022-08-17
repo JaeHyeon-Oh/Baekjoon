4e8765846e817eb697c9239705b80e94d25c21be
Jaehyeon Oh <dhwogus1212@naver.com>
2022-08-17 17:48:57 +0900

    First Union Find

--- 최단 거리,유니온 파인드/맥주 마시면서 걸어가기.py
+++ 최단 거리,유니온 파인드/맥주 마시면서 걸어가기.py
@@ -2,29 +2,27 @@ import sys
 from collections import deque
 sys.stdin=open('D:/Baekjoon/input.txt')
 t=int(sys.stdin.readline())
-q=deque()
+def bfs(x,y):
 
-def bfs(sx,sy):
-    q.append((sx,sy))
-
+    q.append((x,y))
     while q:
-        qx,qy=q.popleft()
-        if abs(qx-ex)+abs(qy-ey)<=1000:
+        px,py=q.popleft()
+        if abs(ex - px) + abs(ey - py) <= 1000:
             print('happy')
             return
         for i in range(n):
-            if not visited[i]:
-                con_x,con_y=conv[i][0],conv[i][1]
-                if abs(qx-con_x)+abs(qy-con_y)<=1000:
-                    visited[i]=1
-                    q.append((con_x,con_y))
+            if not visited[i] and abs(conv[i][0]-px) +abs(conv[i][1]-py)<=1000:
+                visited[i]=1
+                q.append((conv[i][0],conv[i][1]))
+
     print('sad')
-    return
+
 
 for _ in range(t):
+    q = deque()
     n=int(sys.stdin.readline())
+    visited = [0] * (n)
     sx,sy=map(int,sys.stdin.readline().split())
     conv=[list(map(int,sys.stdin.readline().split())) for _ in range(n)]
     ex,ey=map(int,sys.stdin.readline().split())
-    visited=[0]*(n)
     bfs(sx,sy)
--- 최단 거리,유니온 파인드/집합의 표현.py
+++ 최단 거리,유니온 파인드/집합의 표현.py
@@ -1,15 +1,35 @@
 import sys
+sys.setrecursionlimit(10000)
 sys.stdin=open('D:/Baekjoon/input.txt')
 n,m=map(int,sys.stdin.readline().split())
-s=[[i] for i in range(n+1)]
-for _ in range(m):
-    x,y,z=map(int,sys.stdin.readline().split())
-    if not x:
-        s[y]+=s[z]
-        s[z]=s[y]
+parent=[i for i in range(n+1)]
+
+
+def find(x):
+    if parent[x] == x:
+        return x
+    return find(parent[x])
+
+def union(x,y):
+    nx=find(x)
+    ny=find(y)
+
+    if nx==ny:
+        return
+    if nx<ny:
+        parent[nx]=ny
     else:
-        if z in s[y]:
+        parent[ny]=nx
+
+for _ in range(m):
+    check,x,y=map(int,sys.stdin.readline().split())
+    if check:
+        a=find(x)
+        b=find(y)
+        if a==b:
             print('YES')
         else:
             print('NO')
+    else:
+        union(x,y)
 
